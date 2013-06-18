30d8131486bef4e082f95767b080fd3fc71257ea
Aykut Akin <aykutakin90@gmail.com>
2013-06-18 09:49:17 +0300

    Adds samples file

---
+++ Samples.sql
@@ -0,0 +1,14 @@
+
+------------------Samples---------------------
+
+BEGIN
+  dbms_output.put_line(PKG_TABLE_TO_HTML.TABLE_TO_HTML('HR.EMPLOYEES','My header'));
+END;
+
+----------------------------------------------
+
+BEGIN
+  dbms_output.put_line(PKG_TABLE_TO_HTML.SQL_TO_HTML('SELECT EMPLOYEE_ID, FIRST_NAME || ' ' || LAST_NAME AS FULL_NAME FROM HR.EMPLOYEES','My header'));
+END;
+
+----------------------------------------------
